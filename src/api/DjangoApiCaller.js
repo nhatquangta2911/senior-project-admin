4385952382dfd8f772acc550c9da99b443e5c91a
nhatquangta2911 <nhatquangta2911@gmail.com>
2019-12-09 00:56:08 +0700

    Change API endpoint (domain changing)

--- src/api/DjangoApiCaller.js
+++ src/api/DjangoApiCaller.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://model.shawnta.tech:8000/api/";
+const baseUrl = "http://admin.shawnta.tech:8000/api/";
 // const baseUrl = "http://127.0.0.1:8000/api/";
 
 export default function Caller(endpoint, method = "GET", body = {}) {
