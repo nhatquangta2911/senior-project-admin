7b4bd4df9fe55ffdda3ca982c2486deedc0044bb
nhatquangta2911 <nhatquangta2911@gmail.com>
2019-12-15 23:04:38 +0700

    Change port to default

--- src/api/ModelApiCaller.js
+++ src/api/ModelApiCaller.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseUrl =
-  'https://oakhosndol.execute-api.us-east-1.amazonaws.com/dev/api';
+  "https://oakhosndol.execute-api.us-east-1.amazonaws.com/dev/api";
 
 export default function Caller(
   endpoint,
-  method = 'GET',
+  method = "GET",
   body = {},
-  id = '',
-  token = ''
+  id = "",
+  token = ""
 ) {
   return axios(`${baseUrl}/${endpoint}`, {
     method: method,
